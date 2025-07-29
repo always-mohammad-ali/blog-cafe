b12195585200ba41492deb094a12552cc961cf96
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-07-29 19:50:57 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl'>Blog Cafe</h1>
+      <p className=''></p>
+    </>
+  )
+}
+
+export default App
